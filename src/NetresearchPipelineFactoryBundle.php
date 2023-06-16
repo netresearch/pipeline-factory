7058cf4134c9931f51cf0700322cd090e5529da8
Paul Siedler <paul.siedler@netresearch.de>
2023-06-16 12:30:54 +0200

    HMKG-183: Initial commit

---
+++ src/NetresearchPipelineFactoryBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Netresearch\PipelineFactoryBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NetresearchPipelineFactoryBundle extends Bundle
+{
+}
